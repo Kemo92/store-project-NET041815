45ab3424b9817c1bc4c3c9c9bc9e2757022a7f0c
Sarah R <phoesarah@hotmail.com>
2015-06-13 09:41:16 -0500

    completed issue 4

---
+++ Tests/Core.Tests/StoreTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using StoreProject.Core;
+
+namespace StoreProjects.Core.Tests
+{
+    public class StoreTests
+    {
+        [Fact]
+        public void FindProduct_ReturnsProduct_IfPartialSKUisInput()
+        {
+            var store = new Store();
+            var find = store.FindProduct("NV");
+            find.Sku.Should().Be("NV970");
+        }
+
+        [Fact]
+        public void FindProduct_ReturnsProduct_ifSKUisLowercase()
+        {
+            var store = new Store();
+            var find = store.FindProduct("nv");
+            find.Sku.Should().Be("NV970");
+
+        }
+    }
+}
